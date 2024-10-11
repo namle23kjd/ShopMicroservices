6687fa1affae6b22101dac3869e0bc7c93acb501
NET_13_LeXuanPhuongNam <namlxpse173365@fpt.edu.vn>
2024-10-11 10:30:04 +0700

    Get Product

---
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/GetProduct/GetProductsEndpoint.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Catalog.API.Products.GetProduct;
+
+//publiv record GetProductsRequest();
+
+public record GetProductReponse(IEnumerable<Product> Products);
+
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+            var reponse = result.Adapt<GetProductReponse>();
+            return Results.Ok(reponse);
+        })
+        .WithName("GetProducts")
+        .Produces<GetProductReponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products")
+        .WithDescription("Get Products");
+    }
+}
